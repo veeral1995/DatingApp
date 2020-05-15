25d084ca382ec5d2f9c8dd3355ea138f7fd3cfae
Veeral Patel <vpatel@medicomhealth.com>
2020-05-15 11:44:04 -0400

    adding Member Edit

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -25,6 +25,8 @@ import { AuthGuard } from './_guards/auth.guard';
 import { UserService } from './_services/user.service';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -47,7 +49,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
     ListsComponent,
     MessagesComponent,
     MemberCardComponent,
-    MemberDetailComponent
+    MemberDetailComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
@@ -66,7 +69,9 @@ export class CustomHammerConfig extends HammerGestureConfig {
       }})
   ],
   providers: [AuthService, ErrorInterceptorProvider, AlertifyService, AuthGuard, UserService, MemberDetailResolver, MemberListResolver,
-  { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig} ],
+  { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig},
+    MemberEditResolver
+],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
